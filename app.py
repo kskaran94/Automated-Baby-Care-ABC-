56b3a95128e9d29593066f623b7e85f0a2ab6e6f
kskaran94 <kskaransindwani94@gmail.com>
2019-04-29 19:34:14 -0400

    Main file added

---
+++ app.py
@@ -0,0 +1,27 @@
+from image_process import image_process
+from video_util import video_to_frames
+import pygame
+
+
+def initialize():
+	pygame.init()
+	cry_status = 0
+
+	return cry_status
+
+
+def main():
+    cry_status = initialize()
+    video_url = 'video0.mp4'
+    frame_list = video_to_frames(video_url)
+
+    #video to frames
+
+    for i in image_process(frame_list, cry_status):
+    	if i == 1:
+    		print("Status is crying")
+    	else:
+    		print("Status is not crying")
+
+
+main()
---
+++ config.py
@@ -0,0 +1,2 @@
+subscription_key = '1691e8e96bc64328aa7de93fd68b8c5e'
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
