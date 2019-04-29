35907791a9f14a8b6d3a7d0184d9f0580549856e
kskaran94 <kskaransindwani94@gmail.com>
2019-04-29 19:54:00 -0400

    Old file deleted

--- video_to_image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('y2mate.com - baby_cryingmp4_1V5VPlmhdpc_360p.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-    cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-    success,image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 1
