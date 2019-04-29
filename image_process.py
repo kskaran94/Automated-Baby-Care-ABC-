55ba3f46426a4089cff328a1c8a90d3d2fbb5233
kskaran94 <kskaransindwani94@gmail.com>
2019-04-29 19:36:27 -0400

    image processing function added

---
+++ image_process.py
@@ -0,0 +1,47 @@
+import json
+import requests
+from music_service import play_music, stop_music
+from email_service import email_service
+from config import subscription_key, face_api_url
+
+## image_process processes an image and detect the emotion of the child based on
+# threshold and returns a status.
+
+
+def image_process(frame_list, cry_status):
+
+    for i in range(0, len(frame_list), 100):
+        path_to_file = frame_list[i]
+        with open(path_to_file, 'rb') as f:
+            data = f.read()
+
+        headers = dict()
+        headers['Ocp-Apim-Subscription-Key'] = subscription_key
+        headers['Content-Type'] = 'application/octet-stream'
+
+
+        params = {
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'emotion',
+        }
+
+        response = requests.post(face_api_url, params=params, headers=headers,
+                                data=data)
+
+        sad_coeff = response.json()[0]['faceAttributes']['emotion']['sadness']
+        if sad_coeff > 0.7: #pre-defined threshold
+            if not cry_status:
+                #play music
+                #send notification
+                play_music()
+                email_service()
+                cry_status = 1
+
+        else:
+            if cry_status:
+                #stop music
+                stop_music()
+                cry_status = 0
+
+        yield cry_status
