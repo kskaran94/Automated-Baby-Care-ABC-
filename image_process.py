5d1aa1c79b0408b0825190fc18f7a17646113ff6
Lisa Sarah Thomas <lisa@dyn-160-39-216-62.dyn.columbia.edu>
2019-05-08 18:23:55 -0400

    Changed method signature for music service

--- image_process.py
+++ image_process.py
@@ -1,9 +1,9 @@
 import json
 import requests
-from music_service.service import play_music, stop_music
+from music_service.service import start_music, stop_music
 from email_service.service import email_service
 from config import subscription_key, face_api_url
-
+import time
 ## image_process processes an image and detect the emotion of the child based on
 # threshold and returns a status.
 
@@ -32,18 +32,12 @@ def image_process(frame_list, cry_status):
         sad_coeff = response.json()[0]['faceAttributes']['emotion']['sadness']
         if sad_coeff > 0.7: #pre-defined threshold
             if not cry_status:
-                #play music
-                #send notification
-                music_process = play_music()
+                music_process = start_music()
                 email_service()
                 cry_status = 1
-
         else:
             if cry_status:
-                #stop music
                 stop_music(music_process)
                 cry_status = 0
-
         yield cry_status
-        
     music_process.terminate()
