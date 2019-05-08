81f47efe38073b73074c63d28c7455c73b98deee
Lisa Sarah Thomas <lisa@dyn-160-39-216-62.dyn.columbia.edu>
2019-05-08 18:23:02 -0400

    Subscription renewed

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-subscription_key = '1691e8e96bc64328aa7de93fd68b8c5e'
+subscription_key = '7798d94f73ee456ba1fa5d9797012b45'
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 email_username = 'automatedbabycare@gmail.com'
 email_password = 'babycrying'
