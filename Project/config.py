c61f6cb6cc721880fb2290283e2c97ec49db4ed7
kskaran94 <kskaransindwani94@gmail.com>
2019-05-13 12:27:27 -0400

    updated api key

--- Project/config.py
+++ Project/config.py
@@ -1,4 +1,4 @@
-subscription_key = '7798d94f73ee456ba1fa5d9797012b45'
+subscription_key = 'e107a12d478e47a9b3176b0583831ebb'
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 email_username = 'automatedbabycare@gmail.com'
 email_password = 'babycrying'
